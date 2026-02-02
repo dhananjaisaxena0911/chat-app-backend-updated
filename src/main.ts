a0b6f2a4de3b8c590f810c3d5c93f4bfa7b1a857
Dhananjai <dhananjai.saxena@mail.vinove.com>
2026-02-02 14:38:52 +0530

    `Update CORS configuration to use environment variable for frontend URL`

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin:'http://localhost:3000',
-    credentials:true,
-    methods:['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   })
   app.useGlobalPipes(new ValidationPipe());
