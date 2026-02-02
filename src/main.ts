9bd657c508057f36bc713be41376bd68ca7ef62c
Dhananjai <dhananjai.saxena@mail.vinove.com>
2026-02-02 17:24:48 +0530

    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  })
+  origin: [
+    'https://chat-app-frontend-v3.vercel.app',
+  ],
+  credentials: true,
+});
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3001);
 }
