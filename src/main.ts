539d66b179b5b5a74a036aefb72abf474b7fadaf
Dhananjai <dhananjai.saxena@mail.vinove.com>
2026-02-03 10:45:10 +0530

    `Update CORS configuration in main.ts`

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-  origin: [
-    'https://chat-app-frontend-v3.vercel.app',
-  ],
-  credentials: true,
-});
+    origin: [
+      'http://localhost:3000',
+      'http://127.0.0.1:3000',
+      'http://192.168.2.211:3000',
+      'https://chat-app-frontend-v3.vercel.app',
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 3001);
 }
+
 bootstrap();
